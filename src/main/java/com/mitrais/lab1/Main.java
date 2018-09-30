0f5b1596fc3d26f990111ad62bf4e2524f58ea93
Javier Chacana <javierchacana@gmail.com>
2018-09-30 20:07:07 +0800

    First commit

---
+++ src/main/java/com/mitrais/lab1/Main.java
@@ -0,0 +1,11 @@
+package com.mitrais.lab1;
+
+/**
+ * Created by jchacana on 9/30/18.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is my Main class");
+    }
+}
---
+++ src/main/java/com/mitrais/lab1/model/Cake.java
@@ -0,0 +1,22 @@
+package com.mitrais.lab1.model;
+
+public abstract class Cake {
+
+    protected String name;
+    protected double rate;
+
+    public Cake (String name, double rate) {
+        this.name = name;
+        this.rate = rate;
+    }
+
+    public abstract double calcPrice();
+
+    @Override
+    public String toString() {
+        return "com.mitrais.lab1.model.Cake{" +
+                "name='" + name + '\'' +
+                ", rate=" + rate +
+                '}';
+    }
+}
